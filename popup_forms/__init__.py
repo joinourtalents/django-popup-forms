6045ab021491243f24878921fbb4cc67c550bf90
David Avsajanishvili <avsd05@gmail.com>
2012-03-11 10:59:09 +0400

    Setuptools configured

---
+++ popup_forms/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.9.0'
+__author__ = 'David Avsajanishvili'
+__copyright__ = 'Copyright (c) Social TRM Ltd.'
+__license__ = 'BSD'
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+try:
+    long_description = open('README.rst').read()
+except IOError:
+    long_description = ''
+
+setup(
+    name='django-popup-forms',
+    version='0.9',
+    description='Django pop-up forms framework.',
+    long_description=long_description,
+    author='Social TRM Ltd',
+    author_email='david@socialtrm.com',
+    url='http://github.com/joinourtalents/django-popup-forms',
+    keywords = "django",
+    packages=['popup_forms'],
+    include_package_data=True,
+    zip_safe=False,
+    license='BSD License',
+    platforms = ['any'],
+)
