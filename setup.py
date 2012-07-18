2f62f467e7a10a345e99e76a932af4947238fbd4
David Avsajanishvili <avsd05@gmail.com>
2012-07-18 16:55:48 +0400

    Add manifest file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setup(
     name='django-popup-forms',
-    version='1.0.2',
+    version='1.0.3',
     description='Django pop-up forms framework.',
     long_description=long_description,
     author='Social TRM Ltd',
